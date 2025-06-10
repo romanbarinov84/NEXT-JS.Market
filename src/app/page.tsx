7772b31fa4bce91c68fdab0c8a6684ad71c18b51
Roman <roman-meinl@ukr.net>
2025-06-10 11:41:56 -0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <h1 style={{fontSize:"50px"}}>Head Page</h1>
+    </div>
+  );
+}
