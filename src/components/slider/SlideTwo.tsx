ecf4fc38e8e3d7ea10e368cb8a68beeba9f26e81
Roman <roman-meinl@ukr.net>
2025-06-11 11:18:04 -0200

    create slider finish

--- src/components/slider/SlideTwo.tsx
+++ src/components/slider/SlideTwo.tsx
@@ -6,7 +6,7 @@ export function SlideTwo(){
 
     return(
         <div className="mb-10 md:mb-15 xl:mb-20 h-20 md:h-40 xl:h-70 w-full relative">
-        <Image src="/slider/sliderTwo.png" alt="SlideTwo-image" fill className="object-cover"/> 
+        <Image src="/slider/sliderTwo.png" alt="SlideTwo-image" fill priority className="object-cover"/> 
 
         </div>
     )
