a4b1140da4b6ddd6a17a0a201ea165e66690bd53
Roman <roman-meinl@ukr.net>
2025-06-11 09:19:39 -0200

    create Slider.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-    <h1 style={{fontSize:"50px"}}>Head Page</h1>
+    <main style={{fontSize:"50px"}}>Head Page</main>
     </div>
   );
 }
---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,9 @@
+
+export function Slider(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
