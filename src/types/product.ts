9b2492add5821841a7689cb3fbdc2608f42a20c9
Roman <roman-meinl@ukr.net>
2025-06-11 12:46:39 -0200

    create products card

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface ProductCardProps {
+    id: number;
+    img:string;
+    title:string;
+    description:string;
+    basePrice:number;
+    discountPercent:number;
+    reiting:number;
+    weight?:string;
+    value?:string;
+}
+
