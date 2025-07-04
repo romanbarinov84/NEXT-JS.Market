93ff078e51e92056e06b4237d99b4a78f35239c5
Roman <roman-meinl@ukr.net>
2025-07-04 15:57:57 -0200

    fetchProducts+randomLimit__products(route.ts)+query(pipeLine)

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,16 +1,34 @@
 import { NextResponse } from "next/server";
 import { getDb } from "../../../../utils/api-routes";
 export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 export async function GET(request:Request) {
   try {
-    const category = new URL(request.url).searchParams.get("category")
+    const db = await getDb();
+    const url = new URL(request.url)
+    const category = url.searchParams.get("category")
+    const randomLimit = url.searchParams.get("randomLimit")
+
     if(!category){
         return NextResponse.json(
             {message: "Параметр категории обязателен"},
             {status:400}
         );
-    }
+    };
+
+     const query = {
+      categories:category,
+      quantity: {$gt: 0},
+     };
+
+     if(randomLimit){
+      const pipeLine = [{$match:query}, {$sample:{size: parseInt(randomLimit)}}]
+
+      const products = await db.collection("products").aggregate(pipeLine).toArray();
+      return NextResponse.json(products);
+     }
+
     const products = await(await getDb()).collection("products").find({ categories: { $in: [category] } }).toArray();
     
     return NextResponse.json(products);
