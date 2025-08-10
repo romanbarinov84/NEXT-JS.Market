6f0f7d5bba76d10d5d12b31c55dd6c0551b6905a
Roman Meinl <roman-meinl@ukr.net>
2025-08-10 13:55:13 -0200

    create fetchCategory.ts 10.08 | 13:00

--- src/app/(category)/category/[category]/page.tsx
+++ src/app/(category)/category/[category]/page.tsx
@@ -1,21 +1,42 @@
-import ErrorComponent from "@/components/errorComponent/ErrorComponent";
 
-export default async function CategoryPage({params}:{params:Promise<{category:string}>}){
+import GenericListPage from "@/app/(products)/GenericListPage";
+import Loader from "@/components/Loader";
+import { Suspense } from "react";
+import { PATH_TRANSLATIONS } from "../../../../../utils/pathTranslations";
+import fetchCategory from "../fetchCategory";
+
+
+export async function generateMetaData({
+  params,
+}: {
+  params: Promise<{ category: string }>
+}) {
+  const category = await params;
+  return {
+    title: PATH_TRANSLATIONS[category.category] || category.category,
+    description:`Описание категории товаров : ${PATH_TRANSLATIONS[category.category] || category.category}`
+  };
+}
+export default async function CategoryPage({searchParams,params}:{
+  searchParams:Promise<{page?:string;itemsPerPage?:string;}>
+  params:Promise<{category:string}>}){
      
-    let category:string = "";
+    const {category} = await params;
 
-    try{
-       category = (await params).category
-    }catch(error) {
-        return (
-          <ErrorComponent error={error instanceof Error ? error : new Error(String(error))}
-           userMessage="Ошибка получения категорий"/>
-          
-        );
-      }
-    return(
-        <div>
-        {category}
-        </div>
-    )
+    return (
+    <Suspense fallback={<Loader/>}>
+
+      <GenericListPage
+      searchParams={searchParams}
+      props={{
+        fetchData: ({ pagination: { startIdx, perPage } }) => fetchCategory("category", { pagination: { startIdx, perPage } }),
+        pageTitle: PATH_TRANSLATIONS[category] || category,
+        basePath: `/category/${category}`,
+        contentType:"category",
+        errorMessage: 'Категория не найденна',
+      }}
+    />
+    </Suspense>
+    
+  );
 }
