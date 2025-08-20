ca7b59287f19dc4ca5faf53b130bc802924aad5b
Roman Meinl <roman-meinl@ukr.net>
2025-08-20 12:50:48 -0200

    20.08 | 13:00 form regitstration l-27

--- src/app/(auth)/(registration)/register/page.tsx
+++ src/app/(auth)/(registration)/register/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Image from "next/image";
 import PhoneInput from "../PhoneInput";
 import PersonInput from "../PersonInput";
+import PasswordInput from "../PasswordInput";
 
 const initialFormData = {
   phone: "+380",
@@ -22,12 +23,12 @@ const initialFormData = {
 
 const RegisterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [formData, setFormData] = useState(initialFormData);
   const [error, setError] = useState<{
     error: Error;
     userMessage: string;
   } | null>(null);
-
+  const [formData, setFormData] = useState(initialFormData);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
   const handleClose = () => {
@@ -35,64 +36,87 @@ const RegisterPage = () => {
     router.back();
   };
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const {id,type} = e.target;
-    setFormData((prev) => ({...prev,[id]:value}))
-    const value = e.target.value
-  }
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { id, value } = e.target;
+    setFormData((prev) => ({ ...prev, [id]: value }));
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
+    //
   };
 
   return (
-    <div className="fixed inset-0 z-99 flex items-center justify-center bg-[rgba(179,175,174,0.8)] min-h-screen text-[#414141]">
-      <div
-        className="bg-white rounded shadow-[var(--shadow-auth-form)] 
-             w-full max-w-[568px] max-h-[100vh] overflow-y-auto"
-      >
+    <div className="fixed inset-0 z-100 flex items-center justify-center bg-[#fcd5bacc] min-h-screen text-[#414141]">
+      <div className="bg-white rounded shadow-(--shadow-auth-form) w-full max-w-[687px] max-h-[100vh] overflow-y-auto">
         <div className="flex justify-end">
           <button
-            className="rounded duration-300 cursor-pointer mb-8 "
-            aria-label="Закрыть"
-            title="Закрыть"
             onClick={handleClose}
+            className="bg-[#f3f2f1] rounded duration-300 cursor-pointer mb-8"
+            aria-label="Закрыть"
           >
             <Image
               src="/X_SVG.svg"
-              alt="Закрыть окно"
               width={24}
               height={24}
-              className="bg-white"
+              alt="Закрыть"
             />
           </button>
         </div>
-        <h1 className="text-2xl font-bold text-center mb-10">Регістрація</h1>
-        <h2 className="text-lg font-bold text-center mb-6">Обовязкові поля</h2>
+        <h1 className="text-2xl font-bold text-center mb-10">Регистрация</h1>
+        <h2 className="text-lg font-bold text-center mb-6">
+          Обязательные поля
+        </h2>
         <form
           onSubmit={handleSubmit}
           autoComplete="off"
-          className="w-full max-w-[572px] mx-auto max-h-100vh flex flex-col justify-center overflow-y-auto"
+          className="w-full max-w-[552px] mx-auto max-h-100vh flex flex-col justify-center overflow-y-auto"
         >
           <div className="w-full flex flex-row flex-wrap justify-center gap-x-8 gap-y-4">
             <div className="flex flex-col gap-y-4 items-start">
-              <PhoneInput value={formData.phone} onChangeAction={handleChange}/> 
-              <PersonInput 
-              id="surname" 
-              label="Фамилия" 
-              value={formData.surname} 
-              onChange={handleChange}
+              <PhoneInput
+                value={formData.phone}
+                onChangeAction={handleChange}
               />
-              <PersonInput 
-              id="firstname" 
-              label="Имя" 
-              value={formData.firstName} 
-              onChange={handleChange}
+              <PersonInput
+                id="surname"
+                label="Фамилия"
+                value={formData.surname}
+                onChange={handleChange}
               />
-              Surname Password currentPassword
+              <PersonInput
+                id="firstName"
+                label="Имя"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+              <PasswordInput
+                id="password"
+                label="Пароль"
+                value={formData.password}
+                onChangeAction={handleChange}
+                showPassword={showPassword}
+                togglePasswordVisibilityAction={() =>
+                  setShowPassword(!showPassword)
+                }
+                showRequirements={true}
+              />
+              <PasswordInput
+                id="confirmPassword"
+                label="Подтвердите пароль"
+                value={formData.confirmPassword}
+                onChangeAction={handleChange}
+                showPassword={showPassword}
+                togglePasswordVisibilityAction={() =>
+                  setShowPassword(!showPassword)
+                }
+                compareWidth={formData.password}
+              />
+              
             </div>
             <div className="flex flex-col gap-y-4 items-start">
-              birthDay local City Gender
+              Дата рождения Регион Населенный пункт Пол
             </div>
           </div>
         </form>
