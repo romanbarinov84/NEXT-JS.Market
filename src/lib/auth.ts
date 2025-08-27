88663ec4ef05f4d6b72d34f77635ad472d1c8860
Roman Meinl <roman-meinl@ukr.net>
2025-08-27 12:52:28 -0200

    create app/lib/auth-clients and auth.ts and load resend and better.auth 27.08 | 13:00

---
+++ src/lib/auth.ts
@@ -0,0 +1,39 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+import { Resend } from "resend";
+
+const client = new MongoClient(process.env.DELIVERY_SHOP_DB_URL!);
+const db = client.db("deliveryShop");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      await Resend.emails.send({
+        from: "Галя Балуванна <onboarding@resend.dev>",
+        to: user.email,
+        subject: "Подтвердите email",
+        react: "<strong>It works!</strong>",
+      });
+    },
+    autoSignInAfterVerification: false,
+    expiresIn: 3600,
+  },
+  user: {
+    additionalFields: {
+      phoneNumber: { type: "string", input: true, required: true },
+      surName: { type: "string", input: true, required: true },
+      birthdayDate: { type: "date", input: true, required: true },
+      region: { type: "string", input: true, required: true },
+      location: { type: "string", input: true, required: true },
+      card: { type: "string", input: true, required: false },
+      hasCard: { type: "boolean", input: true, required: false },
+    },
+  },
+});
