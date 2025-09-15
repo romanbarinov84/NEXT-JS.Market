164c94252152f6a104d96ee67bec764332c42eb4
Roman Meinl <roman-meinl@ukr.net>
2025-09-15 13:41:01 -0200

    create file app/admin/style.ts 15.09 | 14:00

--- src/app/(admin)/administrator/page.tsx
+++ src/app/(admin)/administrator/page.tsx
@@ -1,5 +1,21 @@
+import { buttonStyles } from "@/app/(auth)/styles";
+import { Link } from "lucide-react";
+
 const AdminPanel = () => {
-  return <>Панель администратора</>;
+  return <>
+   <div className="p-6">
+      <h1 className="text-2xl text-main-text font-bold mb-6">Панель управления</h1>
+
+      <div className="grid gap-4">
+        <Link
+          href="/administrator/users-list"
+          className={`${buttonStyles.active} [&&]:justify-start px-4 py-2 w-full md:w-1/2`}
+        >
+          Управление пользователями
+        </Link>
+      </div>
+    </div>
+  </>;
 };
 
 export default AdminPanel;
